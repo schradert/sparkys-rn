049aa3f458f255606d1d6036b78cc4d86ad35548
Tristan Schrader <t0rdos@pm.me>
2025-08-24 01:11:29 -0700

    feat: init

---
+++ components/Button.tsx
@@ -0,0 +1,85 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+type Props = {
+	label: string;
+	theme?: "primary";
+	onPress?: () => void;
+};
+
+export default function Button({ label, theme, onPress }: Props) {
+	const isPrimary = theme === "primary";
+	return (
+		<View
+			style={[
+				styles.buttonContainer,
+				!isPrimary
+					? {}
+					: {
+							borderWidth: 4,
+							borderColor: "#ffd33d",
+							borderRadius: 18,
+						},
+			]}
+		>
+			<Pressable
+				onPress={onPress}
+				style={[
+					styles.button,
+					!isPrimary
+						? {}
+						: {
+								backgroundColor: "#fff",
+							},
+				]}
+			>
+				{isPrimary && (
+					<FontAwesome
+						name="picture-o"
+						size={18}
+						color="#25292e"
+						style={styles.buttonIcon}
+					/>
+				)}
+				<Text
+					style={[
+						styles.buttonLabel,
+						!isPrimary
+							? {}
+							: {
+									color: "#25292e",
+								},
+					]}
+				>
+					{label}
+				</Text>
+			</Pressable>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	buttonContainer: {
+		width: 320,
+		height: 68,
+		marginHorizontal: 20,
+		alignItems: "center",
+		justifyContent: "center",
+		padding: 3,
+	},
+	button: {
+		borderRadius: 10,
+		width: "100%",
+		height: "100%",
+		alignItems: "center",
+		justifyContent: "center",
+		flexDirection: "row",
+	},
+	buttonIcon: {
+		paddingRight: 8,
+	},
+	buttonLabel: {
+		color: "#fff",
+		fontSize: 16,
+	},
+});
---
+++ devenv.nix
@@ -0,0 +1,70 @@
+{pkgs, ...}: {
+  android = {
+    enable = true;
+    reactNative.enable = true;
+    buildTools.version = ["35.0.0"];
+    ndk.version = ["27.1.12297006"];
+    platforms.version = ["35"];
+  };
+  languages.nix.enable = true;
+  languages = {
+    typescript.enable = true;
+    javascript.enable = true;
+    javascript.bun.enable = true;
+    javascript.bun.install.enable = true;
+  };
+  packages = with pkgs; [google-cloud-sdk eas-cli];
+
+  git-hooks.default_stages = [
+    "pre-push"
+    "manual"
+  ];
+  git-hooks.hooks = {
+    # pre-commit builtins
+    check-added-large-files.enable = true;
+    check-case-conflicts.enable = true;
+    check-executables-have-shebangs.enable = true;
+    check-merge-conflicts.enable = true;
+    check-symlinks.enable = true;
+    check-vcs-permalinks.enable = true;
+    end-of-file-fixer.enable = true;
+    fix-byte-order-marker.enable = true;
+    forbid-new-submodules.enable = true;
+    mixed-line-endings.enable = true;
+    no-commit-to-branch.enable = true;
+    no-commit-to-branch.settings.branch = ["trunk"];
+    trim-trailing-whitespace.enable = true;
+
+    # third-party
+    commitizen.enable = true;
+    gitleaks = {
+      enable = true;
+      name = "gitleaks";
+      description = "Gitleaks on entire project";
+      entry = "${pkgs.gitleaks}/bin/gitleaks protect --redact";
+    };
+    lychee.enable = true;
+    markdownlint.enable = true;
+    markdownlint.settings.configuration.MD013.line_length = -1;
+    mdsh.enable = true;
+    tagref.enable = true;
+    typos.enable = true;
+
+    # nix
+    alejandra.enable = true;
+    deadnix.enable = true;
+    statix.enable = true;
+    statix.raw.args = [
+      "--config"
+      ((pkgs.formats.toml {}).generate "statix.toml" {
+        disabled = [
+          "unquoted_uri"
+          "repeated_keys"
+        ];
+      })
+    ];
+
+    # javascript
+    biome.enable = true;
+  };
+}
